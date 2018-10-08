fa15cdfd744ba304446a273587bda83e6fca98d2
Tim Phillips <timdphillips@gmail.com>
2018-10-08 19:25:52 +0100

    Reset on rotating clockwise past zero three times

--- lock.js
+++ lock.js
@@ -1,33 +1,47 @@
-const { map, scan, switchMap, pairwise, takeUntil } = rxjs.operators;
-const { fromEvent } = rxjs;
-
-const target = document.getElementById("combo");
+const {
+  bufferCount,
+  combineLatest,
+  distinctUntilChanged,
+  filter,
+  flatMap,
+  map,
+  pairwise,
+  scan,
+  startWith,
+  switchMap,
+  takeUntil,
+  tap,
+  share
+} = rxjs.operators;
+const { fromEvent, interval, of, never } = rxjs;
 
 const mouseDown = fromEvent(document, "mousedown");
 const mouseUp = fromEvent(document, "mouseup");
 const mouseMove = fromEvent(document, "mousemove");
 
-var origin = {
-  x: 200,
-  y: 200
-};
-
 /**
  * Calculates the angle (in degrees) from three points.
  */
 function calculateAngleDegrees(point1, point2, point3) {
-  var point2AtOrigin = {
+  const point2AtOrigin = {
     x: point2.x - point1.x,
     y: point2.y - point1.y
   };
-  var point3AtOrigin = {
+  const point3AtOrigin = {
     x: point3.x - point1.x,
     y: point3.y - point1.y
   };
-  var angleOfPoint2 = Math.atan2(point2AtOrigin.y, point2AtOrigin.x);
-  var angleOfPoint3 = Math.atan2(point3AtOrigin.y, point3AtOrigin.x);
+  const angleOfPoint2 = Math.atan2(point2AtOrigin.y, point2AtOrigin.x);
+  const angleOfPoint3 = Math.atan2(point3AtOrigin.y, point3AtOrigin.x);
   return ((angleOfPoint3 - angleOfPoint2) * 180) / Math.PI;
 }
+
+const numberOfTicks = 40;
+const degreesPerTick = 360 / numberOfTicks;
+const origin = {
+  x: 200,
+  y: 200
+};
 
 const rotation = mouseDown.pipe(
   switchMap(() =>
@@ -47,9 +61,92 @@ const rotation = mouseDown.pipe(
       takeUntil(mouseUp)
     )
   ),
-  scan((currentRotation, rotationAdjustment) => currentRotation + rotationAdjustment, 0),
-  map(newRotation => Math.ceil(newRotation / 9) * 9) // 360 / 40 = 9 degrees per number
+  scan((currentRotation, rotationAdjustment) => {
+    const newRotation = currentRotation + rotationAdjustment;
+    if (newRotation >= 360) {
+      return newRotation - 360;
+    }
+    if (newRotation <= -360) {
+      return newRotation + 360;
+    }
+    return newRotation;
+  }, 0),
+  map(newRotation => Math.ceil(newRotation / degreesPerTick) * degreesPerTick),
+  distinctUntilChanged()
 );
+
+const number = rotation.pipe(
+  startWith(0),
+  map(rotation => {
+    const number = Math.abs(rotation / degreesPerTick);
+    return rotation > 0 ? numberOfTicks - number : number;
+  }),
+  pairwise(),
+  switchMap(([previous, next]) => {
+    const ticksBetweenIfCounterclockwise = Math.min(
+      ...[next - previous, next - previous + numberOfTicks, numberOfTicks].filter(num => num >= 0)
+    );
+    const ticksBetweenIfClockwise = Math.min(
+      ...[previous - next, previous - next + numberOfTicks, numberOfTicks].filter(num => num >= 0)
+    );
+
+    const output = [];
+    if (ticksBetweenIfCounterclockwise < ticksBetweenIfClockwise) {
+      while (previous != next) {
+        if (previous === numberOfTicks - 1) {
+          previous = -1;
+        }
+        output.push(++previous);
+      }
+    } else {
+      while (previous != next) {
+        if (previous === 0) {
+          previous = numberOfTicks;
+        }
+        output.push(--previous);
+      }
+    }
+    return of(...output);
+  }),
+  share()
+);
+
+number.subscribe();
+
+const direction = number.pipe(
+  pairwise(),
+  map(([previousNumber, nextNumber]) => {
+    if (previousNumber === nextNumber) {
+      return undefined;
+    }
+    if (previousNumber === 39 && nextNumber === 0) {
+      return "counterclockwise";
+    }
+    if (previousNumber === 0 && nextNumber === 39) {
+      return "clockwise";
+    }
+    return nextNumber >= previousNumber ? "counterclockwise" : "clockwise";
+  }),
+  filter(value => value !== undefined),
+  distinctUntilChanged()
+);
+
+const reset = direction
+  .pipe(
+    switchMap(dir => {
+      if (dir === "clockwise") {
+        return number.pipe(
+          filter(number => number === 0),
+          tap(value => console.log("test 3")),
+          bufferCount(3)
+        );
+      }
+      return never();
+    })
+  )
+  .subscribe(console.log);
+
+const target = document.getElementById("combo");
 
 rotation.subscribe(newRotation => {
   target.setAttribute("transform", `rotate(${newRotation})`);
